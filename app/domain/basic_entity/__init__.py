b1c8288e0215e667c051ddfb44cd427dba38f2d9
DandaraF <dandara-chr@hotmail.com>
2022-03-14 12:03:42 -0300

    create adapter tag and adapter card

---
+++ app/domain/basic_entity/__init__.py
@@ -0,0 +1,30 @@
+from marshmallow import fields
+from uuid import uuid4
+
+from app.domain.basic_value import BasicValue
+
+
+class BasicEntity(BasicValue):
+    def __init__(self, _id=None):
+        self._id = _id or str(uuid4())
+        self.persist_adapter = None
+
+    def set_adapter(self, adapter):
+        self.persist_adapter = adapter
+
+    def save(self):
+        my_id = self.persist_adapter.save(self.to_json())
+        return my_id
+
+    def delete(self):
+        my_id = self.persist_adapter.delete(self._id)
+        return my_id
+
+    def __eq__(self, other):
+        return self._id == other._id
+
+    def __hash__(self):
+        return hash(self._id)
+
+    class Schema(BasicValue.Schema):
+        _id = fields.String(required=True, allow_none=True)
